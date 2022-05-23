fa42c10c5db0e81093628d0cb95e54535892de6c
rishabhdev <rishabh@doc32.in>
2022-05-23 12:20:33 +0530

    ewffw

--- option-store/src/accel/strikes.js
+++ option-store/src/accel/strikes.js
@@ -16,19 +16,28 @@ const getStrikes = (assetPrice, interval = 50) => {
 };
 
 
+const isLastExpiryOfMonth = (date) => {
+  const month = moment(date).month();
+  const nextMonth = moment(date).add(7, 'days').month();
+
+  return month !== nextMonth;
+}
+
+
 const getOptionSymbols = (assetPrice) => {
   const strikes = getStrikes(assetPrice, 50);
   const currentDate = moment();
   const currentExpiryIndex = _.findIndex(expiryDates, (date) => moment(date, 'dddd, DD MMM, YYYY').isAfter(currentDate));
   const nextExpiryIndex = currentExpiryIndex + 1;
-  const currentExpiry = moment(expiryDates[currentExpiryIndex]);
-  const nextExpiry = moment(expiryDates[nextExpiryIndex]);
+  const currentExpiry = moment(expiryDates[currentExpiryIndex], 'dddd, DD MMM, YYYY');
+  const nextExpiry = moment(expiryDates[nextExpiryIndex], 'dddd, DD MMM, YYYY');
+
   const yCurrentExpiry = currentExpiry.format('YY');
-  const mCurrentExpiry = currentExpiry.format('M');
-  const dCurrentExpiry = currentExpiry.format('DD');
+  const mCurrentExpiry = isLastExpiryOfMonth(currentExpiry) ? currentExpiry.format('MMM') : currentExpiry.format('M');
+  const dCurrentExpiry = isLastExpiryOfMonth(currentExpiry) ?  '' : currentExpiry.format('DD');
   const yNextExpiry = nextExpiry.format('YY');
-  const mNextExpiry = nextExpiry.format('M');
-  const dNextExpiry = nextExpiry.format('DD');
+  const mNextExpiry = isLastExpiryOfMonth(nextExpiry) ? nextExpiry.format('MMM') : currentExpiry.format('M');
+  const dNextExpiry = isLastExpiryOfMonth(nextExpiry) ? '' : nextExpiry.format('DD');
   const currentExpiryStrikesCE = strikes.map(strike => ({ type: 'CE', strike, symbol: `NIFTY${yCurrentExpiry}${mCurrentExpiry}${dCurrentExpiry}${strike}CE`, expiry: moment(expiryDates[currentExpiryIndex], 'dddd, DD MMM, YYYY').toISOString() }));
   const currentExpiryStrikesPE = strikes.map(strike => ({ type: 'PE', strike, symbol: `NIFTY${yCurrentExpiry}${mCurrentExpiry}${dCurrentExpiry}${strike}PE`, expiry: moment(expiryDates[currentExpiryIndex], 'dddd, DD MMM, YYYY').toISOString() }));
   const nextExpiryStrikesCE = strikes.map(strike => ({ type: 'CE', strike, symbol: `NIFTY${yNextExpiry}${mNextExpiry}${dNextExpiry}${strike}CE`, expiry: moment(expiryDates[nextExpiryIndex], 'dddd, DD MMM, YYYY').toISOString() }));
